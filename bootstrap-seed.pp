c9034dcd2a8cbff7c7a3ea137e864e68b2e96ac6
ika108 <git@nimporteou.net>
2013-08-02 15:16:30 +0200

    Create bootstrap-seed.pp

---
+++ bootstrap-seed.pp
@@ -0,0 +1,14 @@
+package {
+    'vim':
+        ensure => installed;
+    'vim-puppet':
+        ensure => installed;
+    'puppetmaster-passenger':
+        ensure => installed;
+    'syslog-ng':
+        ensure => installed;
+    'bind9':
+        ensure => installed;
+    'git':
+        ensure => installed;   
+}
